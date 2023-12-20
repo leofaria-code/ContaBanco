7446f520196d6512cf7e2894a572e811fc2ac518
leofaria-code <leofaria.email@gmail.com>
2023-12-19 22:34:12 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+public class ContaTerminal {
+    
+    int numeroConta;
+    String agencia;
+    String nomeCliente;
+    BigDecimal saldo = BigDecimal.valueOf(237.48);
+    
+//    Numero	Inteiro	1021
+//    Agencia	Texto	067-8
+//    Nome Cliente	Texto	MARIO ANDRADE
+//    Saldo	Decimal	237.48
+
+
+
+}
