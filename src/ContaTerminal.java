ecac8ad0df668b10dc6b4985ea0c7f5f5d83bb09
leofaria-code <leofaria.email@gmail.com>
2023-12-19 22:56:46 -0300

    project finished

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,17 +1,32 @@
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class ContaTerminal {
     
-    int numeroConta;
-    String agencia;
-    String nomeCliente;
-    BigDecimal saldo = BigDecimal.valueOf(237.48);
+    public static void main(String[] args) {
+        
+        String nomeCliente;
+        String agencia;
+        int numeroConta;
+        BigDecimal saldo = BigDecimal.valueOf(237.48);
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o seu NOME!");
+        nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o NÚMERO da AGÊNCIA!");
+        agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o NÚMERO da CONTA!");
+        numeroConta = scanner.nextInt();
+        
+        scanner.close();
+        
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco! %n" +
+                "Sua agência é %s e sua conta é %d. %n" +
+                "Seu saldo de R$ %.2f já está disponível para saque!",
+                nomeCliente, agencia, numeroConta, saldo);
+    }
     
-//    Numero	Inteiro	1021
-//    Agencia	Texto	067-8
-//    Nome Cliente	Texto	MARIO ANDRADE
-//    Saldo	Decimal	237.48
-
-
-
 }
